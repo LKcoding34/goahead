a1b6c6cbe81f592e1f33a523c20f3e8137da3534
LKcoding34 <163517667+LKcoding34@users.noreply.github.com>
2025-11-14 17:08:40 +0530

    Update Iris_app.py

--- Iris_app.py
+++ Iris_app.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyMK3lrz+NeI1Cf/2gcYvIdH",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
